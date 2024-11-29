f7efe970d58b791c95315c7f251005855f1116f3
Abdelmajidsabiri <abdlmajidsabiri26@gmail.com>
2024-11-29 07:59:37 -0500

    Create event handles and state

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -6,11 +6,23 @@ class ContentRating extends Component {
   constructor() {
 
     super();
-     this.state = {
-      likes: 0,
-      dislikes: 0
-    };
+    this.state = {
+        likes: 0,
+        dislikes: 0,
 
+        handleLike:() => {
+            this.setState((prevState) => ({
+                likes: prevState.likes + 1
+            }));
+        },
+
+        handleDislike:() => {
+            this.setState((prevState) => ({
+                dislikes: prevState.dislikes + 1
+            }));
+        }
+        
+    };
   }
   render() {
     return (
@@ -20,10 +32,10 @@ class ContentRating extends Component {
             </p>
 
             <div className='rating-buttons'>
-                <button className="like-button">
+                <button className="like-button" onClick={this.state.handleLike}>
                     Like ({this.state.likes})
                 </button>
-                <button className="dislike-button">
+                <button className="dislike-button" onClick={this.state.handleDislike}>
                     Dislike ({this.state.dislikes})
                 </button>
             </div>
